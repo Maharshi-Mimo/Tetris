c139269dfd13935433326e1bed5ba87b0d7af329
Maharshi <maharshid32@gmail.com>
2024-02-02 01:32:46 +0530

    Using the n-2 version (3.59)

--- Build_Infra/provider.tf
+++ Build_Infra/provider.tf
@@ -1,10 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "~> 3.59"
     }
   }
+    required_version = ">= 1.2.0"
 }
 
 # Configure the AWS Provider
