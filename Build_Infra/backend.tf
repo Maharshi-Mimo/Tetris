dff3edb2eb88281339ddec421fdcf8eab812b76c
Maharshi <maharshid32@gmail.com>
2024-02-02 01:31:22 +0530

    modified and corrected the backend.tf file

--- Build_Infra/backend.tf
+++ Build_Infra/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "tetris-project" # Replace with your actual S3 bucket name
     key    = "Tetris/terraform.tfstate"
     region = "ap-south-1"
+    profile = "Maharshi"
   }
 }
